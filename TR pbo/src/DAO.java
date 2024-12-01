bb209eeeb3383cab3d9f9c49309c8d5b7d02fc20
Alifio431 <alifio431@gmail.com>
2024-12-01 21:43:12 +0700

    p

--- TR pbo/src/DAO.java
+++ TR pbo/src/DAO.java
@@ -1,3 +1,32 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DAO {
-    
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/tas";
+        private static final String DB_USER = "root";
+        private static final String DB_PASSWORD = "";
+
+        public boolean checkCredentials(String username, String password) {
+            String query = "SELECT * FROM user_info WHERE username = ? AND password = ?";
+
+            try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+                 PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+                // Set the parameters for the query
+                preparedStatement.setString(1, username);
+                preparedStatement.setString(2, password);
+
+                // Execute the query
+                ResultSet resultSet = preparedStatement.executeQuery();
+
+                // If the result set is not empty, the user credentials are valid
+                return resultSet.next();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return false; // Return false if there is an exception
+            }
+        }
 }
--- TR pbo/src/koneksi.java
+++ TR pbo/src/koneksi.java
@@ -1,3 +1,26 @@
-public class koneksi{
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class koneksi{
+    private static koneksi instance = new koneksi();
+    private static Connection conn;
+
+    public static Connection getConnection() {
+        return conn;
+    }
+
+    public static koneksi getKoneksi() {
+        return instance;
+    }
+
+    public koneksi() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tas", "root", "");
+            System.out.println("koneksi ke database berhasil!");
+        } catch (SQLException | ClassNotFoundException e) {
+            System.err.println("koneksi gagal: " + e.getMessage());
+        }
+    }
 }
